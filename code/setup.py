d657da8c261054539d73532afc3d1527b0018442
Anton Kozyriev <sonara4mus@gmail.com>
2024-09-06 10:29:14 +0300

    fix(code): add README reference to prevent error on build

--- code/setup.py
+++ code/setup.py
@@ -7,43 +7,44 @@ if __name__ == "__main__":
         fullname="Stochastic Quantization for Image Compression",
         description="Lossy Image Compression with Stochastic Quantization",
         long_description="Lossy image compression algorithms play a crucial role in various domains, including network "
-                         "data transfer, data storage, graphics, and image processing. As image information density "
-                         "increases, so do the resources required for processing and transmission. One of the most "
-                         "prominent approaches to address this challenge is color quantization, proposed by Orchard "
-                         "et al. (1991). This technique optimally maps each pixel of an image to a color from a "
-                         "limited palette, maintaining image resolution while significantly reducing information "
-                         "content. Color quantization can be interpreted as a clustering problem (Krishna et al. "
-                         "(1997), Wan (2019)), where image pixels are represented in a three-dimensional space, with "
-                         "each axis corresponding to the intensity of an RGB channel. However, Norkin et al. (2024) "
-                         "demonstrated that scaling traditional algorithms like K-means can be challenging for large "
-                         "data, such as modern images with millions of colors. This paper reframes color quantization "
-                         "as a three-dimensional stochastic transportation problem between the set of image pixels and "
-                         "an optimal color palette, where the number of colors is a predefined hyperparameter. We "
-                         "employ Stochastic Quantization (SQ) with a seeding technique proposed by Arthur et al. "
-                         "(2007) to enhance the scalability of color quantization. This method introduces a "
-                         "probabilistic element to the quantization process, potentially improving efficiency and "
-                         "adaptability to diverse image characteristics. To demonstrate the efficiency of our "
-                         "approach, we present experimental results using images from the ImageNet dataset. These "
-                         "experiments illustrate the performance of our SQ-based method in terms of compression "
-                         "quality, computational efficiency, and scalability compared to traditional color "
-                         "quantization techniques.",
+        "data transfer, data storage, graphics, and image processing. As image information density "
+        "increases, so do the resources required for processing and transmission. One of the most "
+        "prominent approaches to address this challenge is color quantization, proposed by Orchard "
+        "et al. (1991). This technique optimally maps each pixel of an image to a color from a "
+        "limited palette, maintaining image resolution while significantly reducing information "
+        "content. Color quantization can be interpreted as a clustering problem (Krishna et al. "
+        "(1997), Wan (2019)), where image pixels are represented in a three-dimensional space, with "
+        "each axis corresponding to the intensity of an RGB channel. However, Norkin et al. (2024) "
+        "demonstrated that scaling traditional algorithms like K-means can be challenging for large "
+        "data, such as modern images with millions of colors. This paper reframes color quantization "
+        "as a three-dimensional stochastic transportation problem between the set of image pixels and "
+        "an optimal color palette, where the number of colors is a predefined hyperparameter. We "
+        "employ Stochastic Quantization (SQ) with a seeding technique proposed by Arthur et al. "
+        "(2007) to enhance the scalability of color quantization. This method introduces a "
+        "probabilistic element to the quantization process, potentially improving efficiency and "
+        "adaptability to diverse image characteristics. To demonstrate the efficiency of our "
+        "approach, we present experimental results using images from the ImageNet dataset. These "
+        "experiments illustrate the performance of our SQ-based method in terms of compression "
+        "quality, computational efficiency, and scalability compared to traditional color "
+        "quantization techniques.",
         version="0.1.0",
         author="Vladimir Norkin, Anton Kozyriev",
         author_email="a.kozyriev@kpi.ua",
         license="MIT",
         url="https://github.com/kaydotdev/sqic",
         platforms="Any",
-        python_requires=">=3.11",
+        python_requires=">=3.8",
         packages=setuptools.find_packages(exclude=["tests"]),
         scripts=[],
         classifiers=[
             "Natural Language :: English",
             "Operating System :: OS Independent",
             "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
-            "Programming Language :: Python :: 3.13",
-            "Programming Language :: Python :: 3.14",
             "Topic :: Multimedia :: Graphics :: Editors :: Raster-Based",
         ],
         keywords=[
--- code/sqic/__main__.py
+++ code/sqic/__main__.py
@@ -9,16 +9,16 @@ def main():
     parser = argparse.ArgumentParser(
         description="Compress an image using stochastic color quantization."
     )
-    parser.add_argument("input_image_path", help="Path to the input image file")
-    parser.add_argument("output_image_path", help="Path to save the compressed image")
+    parser.add_argument("input_image_path", help="path to the input image file")
+    parser.add_argument("output_image_path", help="path to save the compressed image")
     parser.add_argument(
-        "colors", type=int, help="Number of colors to use in the compressed image"
+        "colors", type=int, help="number of colors to use in the compressed image"
     )
     parser.add_argument(
         "--random_state",
         type=int,
         default=42,
-        help="Random state for reproducibility (default: 42)",
+        help="random state for reproducibility (default: 42)",
     )
 
     args = parser.parse_args()
