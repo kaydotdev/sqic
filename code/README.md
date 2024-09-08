b539305f4ca324a2c08148ab5c836882aec7834e
Anton Kozyriev <sonara4mus@gmail.com>
2024-09-08 20:50:11 +0300

    doc(sqic): add missing fields to the package manifest

---
+++ code/README.md
@@ -0,0 +1,3 @@
+# SQIC: Stochastic Quantization for Image Compression
+
+A scalable way to compress image without loss of the resolution.
--- code/setup.py
+++ code/setup.py
@@ -1,40 +1,29 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 if __name__ == "__main__":
     setuptools.setup(
         name="sqic",
         fullname="Stochastic Quantization for Image Compression",
         description="Lossy Image Compression with Stochastic Quantization",
-        long_description="Lossy image compression algorithms play a crucial role in various domains, including network "
-        "data transfer, data storage, graphics, and image processing. As image information density "
-        "increases, so do the resources required for processing and transmission. One of the most "
-        "prominent approaches to address this challenge is color quantization, proposed by Orchard "
-        "et al. (1991). This technique optimally maps each pixel of an image to a color from a "
-        "limited palette, maintaining image resolution while significantly reducing information "
-        "content. Color quantization can be interpreted as a clustering problem (Krishna et al. "
-        "(1997), Wan (2019)), where image pixels are represented in a three-dimensional space, with "
-        "each axis corresponding to the intensity of an RGB channel. However, Norkin et al. (2024) "
-        "demonstrated that scaling traditional algorithms like K-means can be challenging for large "
-        "data, such as modern images with millions of colors. This paper reframes color quantization "
-        "as a three-dimensional stochastic transportation problem between the set of image pixels and "
-        "an optimal color palette, where the number of colors is a predefined hyperparameter. We "
-        "employ Stochastic Quantization (SQ) with a seeding technique proposed by Arthur et al. "
-        "(2007) to enhance the scalability of color quantization. This method introduces a "
-        "probabilistic element to the quantization process, potentially improving efficiency and "
-        "adaptability to diverse image characteristics. To demonstrate the efficiency of our "
-        "approach, we present experimental results using images from the ImageNet dataset. These "
-        "experiments illustrate the performance of our SQ-based method in terms of compression "
-        "quality, computational efficiency, and scalability compared to traditional color "
-        "quantization techniques.",
-        version="0.1.0",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        version="0.2.0",
+        project_urls={
+            "Homepage": "https://github.com/kaydotdev/sqic",
+            "Issues": "https://github.com/kaydotdev/sqic/issues",
+            "Repository": "https://github.com/kaydotdev/sqic.git",
+        },
         author="Vladimir Norkin, Anton Kozyriev",
         author_email="a.kozyriev@kpi.ua",
         license="MIT",
         url="https://github.com/kaydotdev/sqic",
         platforms="Any",
         python_requires=">=3.8",
-        packages=setuptools.find_packages(exclude=["tests"]),
+        include_package_data=True,
+        packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
         scripts=[],
         classifiers=[
             "Natural Language :: English",
